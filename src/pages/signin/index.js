bdb5707787f9f675b239999c2d800a9d40325b4a
guilhermemonteiro15JS <guilherme.monteiro@mindswap.academy>
2023-10-15 13:35:55 +0100

    upgrading signIN and corretion the errors

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './index.css'
+import "./index.css";
 import styled from "styled-components";
 import imagem from "../../assets/3DBooks.jpeg";
 
@@ -22,20 +22,21 @@ const Image = styled.img`
 
 const FormContainer = styled.div`
   flex: 1;
-
 `;
 
-const Form = styled.form` //styledd form
+const Form = styled.form`
+  //styledd form
   display: flex;
   flex-direction: column;
-  margin:20px;
+  margin: 20px;
 `;
 
-const FormGroup = styled.div` //space betwem lines 
-  margin-bottom: 15px; 
-  display:flex;
-  justify-content:center;
-  align-items:center;
+const FormGroup = styled.div`
+  //space betwem lines
+  margin-bottom: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Label = styled.label`
@@ -46,56 +47,59 @@ const Input = styled.input`
   padding: 10px;
   margin-top: 5px;
   margin-left: 10px;
-  display:flex;
+  display: flex;
   align-items: center;
-  justify-content:center;
+  justify-content: center;
 `;
 
 const Button = styled.button`
   padding: 10px 20px;
-  background-color: #30A9D9;
+  background-color: #30a9d9;
   color: white;
   border: none;
   cursor: pointer;
 `;
-
 const SignIn = () => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-  
-    
-    fetch("http://5.22.217.225:8081/api/v1/auth/login", {
-      method: 'POST',
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(formData),
-      redirect: 'follow'
-    })
-      .then(response => response.text())
-      .then(result => {
-        alert('login sucessful:');
-        navigate('/library');
-        console.log(result);
-      })
-      .catch(error => console.log('error', error));
+    let response;
+    try {
+      response = await fetch(
+        "http://5.22.217.225:8081/api/v1/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+          redirect: "follow",
+        }
+      );
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+    if (response?.ok) {
+      console.log('Use the response here!');
+      alert('login sucessful:');
+      navigate('/library');
+    } else {
+      console.log(`HTTP Response Code: ${response?.status}`)
+      alert('login failed')
+      setEmail('')
+      setPassword('')
+    }
   };
-
   return (
     <SigninStyled>
       <ImageContainer>
         <Image src={imagem} alt="Imagem de leitura" />
       </ImageContainer>
-    
+
       <FormContainer>
         <h1 id="titulo_SignIn">Sign In</h1>
         <Form onSubmit={handleLogin}>
@@ -105,8 +109,8 @@ const SignIn = () => {
               type="email"
               name="email"
               placeholder="Digite seu email"
-              value={formData.email}
-              onChange={handleInputChange}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </FormGroup>
@@ -116,17 +120,89 @@ const SignIn = () => {
               type="password"
               name="password"
               placeholder="Digite sua senha"
-              value={formData.password}
-              onChange={handleInputChange}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
           </FormGroup>
           <Button type="submit">Entrar</Button>
         </Form>
       </FormContainer>
-      </SigninStyled>
-   
+    </SigninStyled>
   );
 };
+
+/*const SignIn = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    let response;
+    try {
+      response = await fetch(
+        "http://5.22.217.225:8081/api/v1/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+          redirect: "follow",
+        }
+      );
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+    if (response?.ok) {
+      console.log('Use the response here!');
+      alert('login sucessful:');
+      navigate('/library');
+    } else {
+      console.log(`HTTP Response Code: ${response?.status}`)
+      alert('login failed')
+      setEmail('')
+      setPassword('')
+    }
+  };
+  return (
+    <SigninStyled>
+      <ImageContainer>
+        <Image src={imagem} alt="Imagem de leitura" />
+      </ImageContainer>
+
+      <FormContainer>
+        <h1 id="titulo_SignIn">Sign In</h1>
+        <Form onSubmit={handleLogin}>
+          <FormGroup>
+            <Label>Email:</Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Digite seu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Password:</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Digite sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </FormGroup>
+          <Button type="submit">Entrar</Button>
+        </Form>
+      </FormContainer>
+    </SigninStyled>
+  );
+};*/
 
 export default SignIn;
