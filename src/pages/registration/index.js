aace616a8cbd8daad337334dabeb43dead46f21e
Ricardo Oliveira <ricardinho19@outlook.pt>
2023-10-16 19:00:17 +0100

    registration styling

--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -1,5 +1,62 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import imagem from "../../assets/3DBooks.jpeg";
+import styled from "styled-components";
+import "./index.css";
+
+const RegistrationStyled = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  text-align: center;
+  flex: 1;
+  padding: 2rem;
+`;
+
+const Image = styled.img`
+  max-width: 350px;
+  height: 350px;
+`;
+
+const FormContainer = styled.div`
+  flex: 1;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+`;
+
+const FormGroup = styled.div` 
+  margin-bottom: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  font-weight: 1;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin-top: 5px;
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #30a9d9;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
 
 const Registration = () => {
   const navigate = useNavigate();
@@ -41,47 +98,56 @@ const Registration = () => {
   };
   
   return (
-    <div>
-      <h2>Registration</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">First Name:</label>
-          <input
-            type="text"
-            placeholder="Digite seu nome"
-            id="name"
-            name="name"
-            value={formData.name}
+    <RegistrationStyled>
+      <ImageContainer>
+        <Image src={imagem} alt="Imagem de leitura" />
+      </ImageContainer>
+
+      
+      <FormContainer>
+    <h1 id="titulo_Registration">Registration </h1>
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+            <Label>Name:</Label>
+            <Input
+              type="text"
+              name="name"
+              placeholder="Digite seu nome"
+              value={formData.name}
+              onChange={handleInputChange}
+              require
+            />
+          </FormGroup>
+
+      <FormGroup>
+      <Label>Email:</Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Digite seu email"
+              value={formData.email}
+              onChange={handleInputChange}
+              require
+            />
+
+</FormGroup>
+          <FormGroup>
+            <Label>Password:</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Digite sua senha"
+              value={formData.password}
             onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            placeholder="Digite seu email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            placeholder="Digite sua senha"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Register
-        </button>
-      </form>
-    </div>
+              required
+            />
+          </FormGroup>
+          <Button type="submit">Registrar</Button>
+        </Form>
+      </FormContainer>
+    </RegistrationStyled>
+          
+        
   );
 };
 
