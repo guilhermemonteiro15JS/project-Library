9bd7ebf7f1d4311cca8fe050c71ee0d3c3b35ef2
guilhermemonteiro15JS <guilherme.monteiro@mindswap.academy>
2023-10-18 00:12:56 +0100

    navbar_changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Router, Route, Routes, Link} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import Home from "./pages/home";
 import SignIn from "./pages/signin";
 import Library from "./pages/library";
@@ -29,9 +30,18 @@ const BodyApp = styled.body`
   margin: 0 auto;
 `;
 
-
-
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const updateLoginStatus = (status) => {
+    setLoggedIn(status);
+  };
+  
+  const logout = () => {
+    updateLoginStatus(false);
+  };
+
   return (
     <BrowserRouter>
       <Navbar
@@ -41,47 +51,78 @@ const App = () => {
         variant="dark"
         className="sticky-top"
       >
-        <Navbar.Brand href="/home">Library Store </Navbar.Brand>
+        <Navbar.Brand as={Link} to={loggedIn ? "/home" : "/signIn"}>
+          Library Store
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="nav-bar">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/signIn">SignIn</Nav.Link>
-            <Nav.Link href="/registration">Registration</Nav.Link>
-            <Nav.Link href="/administration">Administration</Nav.Link>
-            <div className="search-container ml-auto">
-              <div className="input-group">
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="mr-sm-2"
-                />
-                <div className="input-group-append">
-                  <Button variant="outline-info" className="custom-button">
-                    <span className="bi bi-search"></span>
-                  </Button>
+            {loggedIn ? (
+              <>
+                <Nav.Link as={Link} to="/home">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/library">
+                  Library
+                </Nav.Link>
+                <Nav.Link as={Link} to="/search">
+                  Search
+                </Nav.Link>
+                <div className="search-container ml-auto">
                 </div>
-              </div>
-            </div>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Container>
-          <Row>
-            <Col>
-              <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/library" element={<Library />} />
-                <Route path="/signIn" element={<SignIn />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/registration" element={<Registration />} />
-                <Route path="/administration" element={<Administration/>} />
-              </Routes>
-            </Col>
-          </Row>
-        </Container>
-        <Footer />
-      </BrowserRouter>
+                <Button
+                  variant="outline-info"
+                  onClick = {()=> {
+                    logout();
+                    navigate('/signIn');
+                  }}
+                  className="custom-button"
+                >
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/home">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signIn">
+                  SignIn
+                </Nav.Link>
+                <Nav.Link as={Link} to="/registration">
+                  Registration
+                </Nav.Link>
+                <Nav.Link as={Link} to="/administration">
+                  Administration
+                </Nav.Link>
+                <div className="search-container ml-auto">
+                </div>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col>
+            <Routes>
+              <Route
+                path="/"
+                element={<SignIn updateLoginStatus={updateLoginStatus} />}
+              />
+              <Route path="/" element={<SignIn />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/signIn" element={<SignIn />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/registration" element={<Registration />} />
+              <Route path="/administration" element={<Administration />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -59,44 +59,41 @@ const Button = styled.button`
   border: none;
   cursor: pointer;
 `;
-const SignIn = () => {
+const SignIn = ({ updateLoginStatus }) => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
     let response;
     try {
-      response = await fetch(
-        "http://5.22.217.225:8081/api/v1/auth/login",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-          redirect: "follow",
-        }
-      );
+      response = await fetch("http://5.22.217.225:8081/api/v1/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+        redirect: "follow",
+      });
     } catch (error) {
       console.error("Error fetching data:", error);
     }
     if (response?.ok) {
-
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       const token = data.data.token;
-      localStorage.setItem('authToken', token);
+      localStorage.setItem("authToken", token);
 
-      alert('login sucessful:');
-      navigate('/library');
+      updateLoginStatus(true);
+      alert("login sucessful:");
+      navigate("/library");
     } else {
-      console.log(`HTTP Response Code: ${response?.status}`)
-      alert('login failed')
-      setEmail('')
-      setPassword('')
+      console.log(`HTTP Response Code: ${response?.status}`);
+      alert("login failed");
+      setEmail("");
+      setPassword("");
     }
   };
   return (
@@ -131,83 +128,16 @@ const SignIn = () => {
             />
           </FormGroup>
           <Button type="submit">Entrar</Button>
+          <div>
+            <h2>Se não estás registado, clica abaixo</h2>
+            <Button onClick={() => navigate("/registration")}>
+              Registro
+            </Button>
+          </div>
         </Form>
       </FormContainer>
     </SigninStyled>
   );
 };
-
-/*const SignIn = () => {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    let response;
-    try {
-      response = await fetch(
-        "http://5.22.217.225:8081/api/v1/auth/login",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-          redirect: "follow",
-        }
-      );
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-    if (response?.ok) {
-      console.log('Use the response here!');
-      alert('login sucessful:');
-      navigate('/library');
-    } else {
-      console.log(`HTTP Response Code: ${response?.status}`)
-      alert('login failed')
-      setEmail('')
-      setPassword('')
-    }
-  };
-  return (
-    <SigninStyled>
-      <ImageContainer>
-        <Image src={imagem} alt="Imagem de leitura" />
-      </ImageContainer>
-
-      <FormContainer>
-        <h1 id="titulo_SignIn">Sign In</h1>
-        <Form onSubmit={handleLogin}>
-          <FormGroup>
-            <Label>Email:</Label>
-            <Input
-              type="email"
-              name="email"
-              placeholder="Digite seu email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label>Password:</Label>
-            <Input
-              type="password"
-              name="password"
-              placeholder="Digite sua senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </FormGroup>
-          <Button type="submit">Entrar</Button>
-        </Form>
-      </FormContainer>
-    </SigninStyled>
-  );
-};*/
 
 export default SignIn;
