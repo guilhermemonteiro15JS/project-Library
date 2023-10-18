e03b9add02f3eb39ba24cd3b91967cd62f6bc9c5
Emanuel Leal <emanuel.s.leal@gmail.com>
2023-10-18 18:44:59 +0100

    new Home page

--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -1,22 +1,26 @@
-import styled from "styled-components";
-import imagem from "../../assets/Prancheta2.png";
+import styled, { keyframes } from 'styled-components';
 
-export const StyledBody = styled.body`
-display: flex;
+export const fadeIn = keyframes`
+  0%, 100% {
+    opacity: 0.3;
+  }
+  50% {
+    opacity: 1;
+  }
 `;
 
-export const StyledHome = styled.div`
-  background-image: url(${imagem}); // Use "imagem" em vez de "img"
-  background-size: auto auto;
-  background-position: center;
-  flex: 1;
-  min-height: 50rem;
+export const HomeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+  color: #333;
+  font-family: 'Arial', sans-serif;
 `;
 
-
-export const TextContent = styled.div`
-  font-family: cursive;
-  text-shadow: 2px 4px 10px blue;
-  font-size: 20px;
-  line-height: 50px;
-`;
+export const ConstructionText = styled.h1`
+  font-size: 2em;
+  text-align: center;
+  animation: ${fadeIn} 2s ease-in-out infinite; 
+  `;
